3107115c7e96b4083ec8da254c4669fa0289086b
Tess-hacker <64714004+Tess-hacker@users.noreply.github.com>
2020-05-03 03:09:14 +0100

    Add files via upload

---
+++ CSV DATA.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+excelfile = pd.ExcelFile('Dummy_data.xlsx')
+dframe1 = excelfile.parse('Dummy_data1')
+print ("DATAFRAME ONE RESULTS")
+print (dframe1)
+
+excelfile = pd.ExcelFile('Dummy_data.xlsx')
+dframe2 = excelfile.parse('Dummy_data2')
+print ("DATAFRAME TWO RESULTS")
+print (dframe2)
+
+excelfile = pd.ExcelFile('Dummy_data.xlsx')
+dframe3 = excelfile.parse('Dummy_data3')
+print ("DATAFRAME THREE RESULTS")
+print (dframe3)
+
+excelfile = pd.ExcelFile('Dummy_data.xlsx')
+dframe4 = excelfile.parse('Dummy_data4')
+print ("DATAFRAME FOUR RESULTS")
+print (dframe4)
+
+excelfile = pd.ExcelFile('Dummy_data.xlsx')
+dframe5 = excelfile.parse('Dummy_data5')
+print ("DATAFRAME FIVE RESULTS")
+print (dframe5)
+
+excelfile = pd.ExcelFile('Dummy_data.xlsx')
+dframe6 = excelfile.parse('Dummy_data6')
+print ("DATAFRAME SIX RESULTS")
+print (dframe6)
+
+excelfile = pd.ExcelFile('Dummy_data.xlsx')
+dframe7 = excelfile.parse('Dummy_data7')
+print ("DATAFRAME SEVEN RESULTS")
+print (dframe7)
+
+excelfile = pd.ExcelFile('Dummy_data.xlsx')
+dframe8 = excelfile.parse('Dummy_data8')
+print ("DATAFRAME EIGHT RESULTS")
+print (dframe8)
+
+excelfile = pd.ExcelFile('Dummy_data.xlsx')
+dframe9 = excelfile.parse('Dummy_data9')
+print ("DATAFRAME NINE RESULTS")
+print (dframe9)
+
+excelfile = pd.ExcelFile('Dummy_data.xlsx')
+dframe10 = excelfile.parse('Dummy_data10')
+print ("DATAFRAME TEN RESULTS")
+print (dframe10)
